5d4903e9674a6b4c42cbcae66943d82f45987ff5
Steven McDermott <stevmc@vsp.com>
2020-02-11 19:26:14 -0800

    changing annotations and implementation of service layer impl

--- src/main/java/com/steven/jokes/springbootjokesapp/services/ChuckNorrisJokeServicempl.java
+++ src/main/java/com/steven/jokes/springbootjokesapp/services/ChuckNorrisJokeServicempl.java
@@ -2,14 +2,21 @@ package com.steven.jokes.springbootjokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * stevmc created on 2/11/20
  */
-@Component
+@Service
 public class ChuckNorrisJokeServicempl implements JokeService {
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+	public ChuckNorrisJokeServicempl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
 	@Override
 	public String getJoke() {
-		return new ChuckNorrisQuotes().getRandomQuote();
+		return chuckNorrisQuotes.getRandomQuote();
 	}
 }
